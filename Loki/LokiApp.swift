9dadf187d6f05879ae4a4d4887df5b2804c26a3f
Tigran Zakaryan <t.zakaryan92@gmail.com>
2026-03-14 10:49:15 +0400

    Initial Commit

---
+++ Loki/LokiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokiApp.swift
+//  Loki
+//
+//  Created by Тигран Закарян on 14.03.26.
+//
+
+import SwiftUI
+
+@main
+struct LokiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
